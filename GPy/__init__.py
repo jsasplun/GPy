59c73fc9b97a647328842e9f22960644b423493f
John Asplund <30665264+jsasplun@users.noreply.github.com>
2023-08-09 12:01:35 -0400

    Removed reliance on outdated code from numpy

--- GPy/__init__.py
+++ GPy/__init__.py
@@ -29,20 +29,6 @@ from .core.parameterization import Param, Parameterized, ObsAr, transformations 
 
 from .__version__ import __version__
 
-from numpy.testing import Tester
-
-with warnings.catch_warnings():
-    warnings.simplefilter('ignore')
-    try:
-        #Get rid of nose dependency by only ignoring if you have nose installed
-        from nose.tools import nottest
-        @nottest
-        def tests(verbose=10):
-            Tester(testing).test(verbose=verbose)
-    except:
-        def tests(verbose=10):
-            Tester(testing).test(verbose=verbose)
-
 def load(file_or_path):
     """
     Load a previously pickled model, using `m.pickle('path/to/file.pickle)'
